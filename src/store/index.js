f9f022e3d68770db61d377a8ea30390822e29e40
Rokas <rookas.rudzenskas@gmail.com>
2021-05-11 11:54:35 +0300

    Rokas

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,20 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    counter: 0
   },
   mutations: {
+    increaseCounter(state) {
+      state.counter++
+    },
+    decreaseCounter(state) {
+      state.counter--
+    },
   },
   actions: {
+  },
+  getters: {
+
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,11 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <div class="counter">
-      {{ counter }}
+      {{ $store.state.counter }}
     </div>
     <div class="buttons">
-      <button @click="decreaseCounter">-</button>
-      <button @click="increaseCounter">+</button>
+      <button @click="$store.commit('decreaseCounter')">-</button>
+      <button @click="$store.commit('increaseCounter')">+</button>
     </div>
   </div>
 </template>
@@ -16,19 +16,6 @@
 <script>
 export default {
   name: 'Home',
-  data() {
-    return {
-      counter: 0
-    }
-  },
-  methods: {
-    increaseCounter() {
-      this.counter++
-    },
-    decreaseCounter() {
-      this.counter--
-    },
-  }
 }
 </script>
 
